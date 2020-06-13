c7b61a38dad75ed82b2b1516dd930ae5f75bbfb1
Ahmed Rezk <ahmed.r.badr@gmail.com>
2020-06-13 12:49:23 +0200

    add config file

--- search_company_id.py
+++ search_company_id.py
@@ -1,45 +1,55 @@
 import base64
+import configparser
 import logging
+import sys
 
 import requests
 from requests.auth import HTTPBasicAuth
 
 
 def save_company_id(company_name, company_id):
-    with open('companies_ids.csv', 'a') as f:
+    with open(ids_files, 'a') as f:
         f.write(f'{company_id},{company_name}\n')
 
 
 def find_company_id(company_name):
     json = {}
     try:
-        response = requests.get(f'{REED_KEYWORD}{company_name}&resultsToTake=5', auth=HTTPBasicAuth(API_KEY, ''))
+        reed_url = config['DEFAULT']['ReedKeywordUrl']
+        response = requests.get(f'{reed_url}{company_name}&resultsToTake=5', auth=HTTPBasicAuth(api_key, ''))
         response.raise_for_status()
         json = response.json()
     except requests.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
+        logging.error(f'HTTP error occurred: {http_err}')
+        logging.error(f'response: {http_err.response.text}')
+        config['DEFAULT']['StartIdSearchFrom'] = company_name
+        sys.exit()
     except Exception as err:
-        print(f'Other error occurred: {err}')
+        logging.error(f'Other error occurred: {err}')
 
     if 'results' not in json:
         logging.error('Unexpected response from Reed')
         logging.error(json)
-        raise Exception()
+        config['DEFAULT']['StartIdSearchFrom'] = company_name
+        return
 
     for result in json['results']:
         if result.get('employerName').lower().strip() == company_name.lower().strip():
+            logging.info(f'an ID is found for company {company_name}')
             return result.get('employerId')
+    logging.info(f'{company_name} not found')
 
 
 def read_api_key():
-    with open(KEY_FILE) as f:
+    key_file = config['DEFAULT']['ApiKeyFile']
+    with open(key_file) as f:
         api_key = f.readline()
     return api_key
 
 
-def store_companies_ids(starting_from=None):
+def store_companies_ids():
     get_started = True if starting_from is None else False
-    with open('tier2_companies.txt') as companies_names:
+    with open(tier_2_names_file) as companies_names:
         companies_names = companies_names.readlines()
         for name in companies_names:
             # remove trailing \n
@@ -53,14 +63,13 @@ def store_companies_ids(starting_from=None):
 
             company_id = find_company_id(name)
             if company_id is not None:
-                logging.info(f'an ID is found for company {name}')
                 save_company_id(name, company_id)
-            logging.info(f'{name} not found')
-            return
+            elif config['DEFAULT']['ContinueOnError'] == 'False':
+                return
 
 
 def generate_auth_header():
-    base64string = base64.b64encode(f'{API_KEY}:'.encode())
+    base64string = base64.b64encode(f'{api_key}:'.encode())
     return b'Authorization: Basic %s' % base64string
 
 
@@ -68,7 +77,7 @@ def init_logger():
     # File to log to
     log_file = 'crawler.log'
     logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        format='%(asctime)s %(name)-8s %(levelname)-8s %(message)s',
                         datefmt='%m-%d %H:%M',
                         filename=log_file,
                         filemode='w')
@@ -84,8 +93,11 @@ def init_logger():
 
 
 if __name__ == '__main__':
-    KEY_FILE = 'REED_API_KEY'
-    REED_KEYWORD = 'https://www.reed.co.uk/api/1.0/search?keywords='
-    API_KEY = read_api_key()
     init_logger()
-    store_companies_ids(starting_from='Al-Emaan Centre')
+    config = configparser.ConfigParser()
+    config.read('crawler.ini')
+    api_key = read_api_key()
+    starting_from = config['DEFAULT']['StartIdSearchFrom']
+    tier_2_names_file = config['DEFAULT']['CompaniesNames']
+    ids_files = config['DEFAULT']['CompaniesIDs']
+    store_companies_ids()
